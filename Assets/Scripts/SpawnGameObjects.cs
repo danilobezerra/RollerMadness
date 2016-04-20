d9d8f2b1e23916dac77e9243995dde22bc9f92cc
Danilo Bezerra <danilobezerra@outlook.com.br>
2016-04-20 13:18:27 -0300

    BaseSpawnGameObjects implemented

--- Assets/Scripts/SpawnGameObjects.cs
+++ Assets/Scripts/SpawnGameObjects.cs
@@ -1,43 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
+using Framework.Template;
 
-public class SpawnGameObjects : MonoBehaviour {
+public class SpawnGameObjects : BaseSpawnGameObjects {
+[SerializeField] private GameObject spawnPrefab;
 
-	public GameObject spawnPrefab;
-
-	public float minSecondsBetweenSpawning = 3.0f;
-	public float maxSecondsBetweenSpawning = 6.0f;
+	[SerializeField] private float minSecondsBetweenSpawning = 3.0f;
+	[SerializeField] private float maxSecondsBetweenSpawning = 6.0f;
 	
-	public Transform chaseTarget;
-	
-	private float savedTime;
-	private float secondsBetweenSpawning;
+	[SerializeField] private Transform chaseTarget;
+    
+    protected override void MakeThingToSpawn() {
+        GameObject clone = SpawnClone(spawnPrefab, transform.position, transform.rotation);
 
-	// Use this for initialization
-	void Start () {
-		savedTime = Time.time;
-		secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Time.time - savedTime >= secondsBetweenSpawning) // is it time to spawn again?
-		{
-			MakeThingToSpawn();
-			savedTime = Time.time; // store for next spawn
-			secondsBetweenSpawning = Random.Range (minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
-		}	
-	}
-
-	void MakeThingToSpawn()
-	{
-		// create a new gameObject
-		GameObject clone = Instantiate(spawnPrefab, transform.position, transform.rotation) as GameObject;
-
-		// set chaseTarget if specified
-		if ((chaseTarget != null) && (clone.gameObject.GetComponent<Chaser> () != null))
-		{
-			clone.gameObject.GetComponent<Chaser>().SetTarget(chaseTarget);
+		var chaser = clone.gameObject.GetComponent<Chaser>();
+		if (chaseTarget != null && chaser != null) {
+			chaser.SetTarget(chaseTarget);
 		}
-	}
+    }
+    
+    protected override float HandleNextSpawn() {
+        return Time.time + Random.Range(minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
+    }
 }
